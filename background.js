dc7060ca30e89de4afe549fac2f08155b2933fc0
asafcc <asaf995@gmail.com>
2020-09-13 12:06:12 +0300

    added additional support for url changes

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 const CHRONOLOGICAL = "?sorting_setting=CHRONOLOGICAL";
-const REGEX = /https:\/\/www.facebook.com\/groups\/([a-zA-Z0-9._]+)\/(\?multi_permalinks=[0-9]+)?$/;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
+    const REGEX = /https:\/\/www.facebook.com\/groups\/([a-zA-Z0-9._]+)\/(\?multi_permalinks=[0-9]+)?$/;
     const result = details.url.match(REGEX);
 
     //checks we are on facebook group url but not the main feed page, and that we've not already added the CHRONOLOGICAL param
@@ -16,3 +16,13 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   ["blocking"]
 );
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+  const REGEX = /https:\/\/www.facebook.com\/groups\/([0-9a-zA-Z._]+)(\/\?multi_permalinks=[0-9]+)?$/;
+  const result = details.url.match(REGEX);
+  //checks we are on facebook group url but not the main feed page, and that we've not already added the CHRONOLOGICAL param
+  if (REGEX.test(details.url) && result[1] !== "feed") {
+    const redirectUrl = `https://www.facebook.com/groups/${result[1]}/${CHRONOLOGICAL}`;
+    chrome.tabs.update({ url: redirectUrl });
+  }
+});
